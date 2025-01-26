1b1a17bd32bef0f34d324d1736e22602a15318e4
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-01-26 19:37:33 -0300

    Teste da sombra

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,6 +3,7 @@ export default function Projects() {
         <section data-aos="fade" className="w-screen h-[1000px] py-20">
             <div className="wrapper flex flex-col items-center pt-10">
                 <h1 className="text-5xl text-cyan-500 font-bold">Meus Projetos</h1>
+                <div className="animated-shadow"></div>
             </div>
         </section>
     );
