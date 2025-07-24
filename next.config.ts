60b0136673d456f3bfe5c84eb3e73df085805129
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-24 20:53:14 -0300

    Novo README e teste de exportação

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	basePath: "/portfolio",
 	output: "export",
 	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
 };
+
+if (process.env.NODE_ENV !== "development") {
+	nextConfig.basePath = "/portfolio";
+}
 
 export default nextConfig;
