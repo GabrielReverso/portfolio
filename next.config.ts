7c1a581caea2059b970ff7cf802011f51424f709
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-24 20:04:23 -0300

    Fix de config e instalação

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
-
 const nextConfig: NextConfig = {
 	output: "export",
 	reactStrictMode: true,
 	images: {
 		unoptimized: true,
 	},
-	...(isDev ? {} : { basePath: "/portfolio" }),
 };
+
+if (process.env.NODE_ENV !== "development") {
+	nextConfig.basePath = "/portfolio";
+}
 
 export default nextConfig;
