207fa717437e8416ef16d8135fd91d46d6d7c528
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-07-24 20:01:07 -0300

    Fix de rota

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
-	basePath: isDev ? "" : "/portfolio",
 	output: "export",
 	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
+	...(isDev ? {} : { basePath: "/portfolio" }),
 };
 
 export default nextConfig;
